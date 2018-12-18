7a7af37896a577c6ede2df147ae7734766477e6e
SandzoNight <flukemdc@gmail.com>
2018-12-18 19:09:37 +0700

    [ADD] Init

---
+++ src/main/java/camp/wip/line/bot/server/botserver/BotServerApplication.java
@@ -0,0 +1,14 @@
+package camp.wip.line.bot.server.botserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotServerApplication.class, args);
+	}
+
+}
+
